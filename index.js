1ab61e6cf7e88363e44edc2171f2d3f4850c5300
theMVPshop <79482771+theMVPshop@users.noreply.github.com>
2021-07-29 18:17:29 -0500

    dfsfs

--- index.js
+++ index.js
@@ -1,42 +1,39 @@
 require('dotenv').config();
 const express = require('express');
-const path = require('path');
-const cors = require('cors');
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
+// const path = require('path');
+// const cors = require('cors');
+// const morgan = require('morgan');
+// const bodyParser = require('body-parser');
 const db = require('./sql/connect');
 
 // const postsRouter = require("./Routers/posts");
 // const clientsRouter = require("./Routers/clients");
 
 const app = express();
-const port = process.env.PORT || 80;
-console.log("process.env.port ", port)
 
 
-app.use(express.static('public'));
 
-app.use(morgan('dev'));
-app.use(cors());
-app.use(bodyParser.urlencoded({ extended: false}));
-app.use(bodyParser.json());
+// app.use(express.static('public'));
+
+// app.use(morgan('dev'));
+// app.use(cors());
+// app.use(bodyParser.urlencoded({ extended: false}));
+// app.use(bodyParser.json());
 
 // app.use("/posts", postsRouter);
 // app.use("/clients", clientsRouter);
-
-app.get("/", (req, res) => {
-  res.send("Welcome!");
-});
-
+app.use(express.json());
+app.get("/", (req, res) => {res.send("Welcome!");});
 app.use('/posts', require('./Routers/posts'));
-app.use('./clients', require('./Routers/clients'));
+
+// app.use('./clients', require('./Routers/clients'));
 
 // app.post("/api/posts", (req, res) =>{
  
 //     res.json(req.body);
 
 // })
-
+const port = process.env.PORT || 5000;
 app.listen(port, () =>
 console.log(`[⚡️ server] app is listening on port ${PORT}`)
 );
