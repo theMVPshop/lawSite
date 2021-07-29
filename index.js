8da182b4fcfa415fc69c096f5102d82ba3cc68fa
theMVPshop <79482771+theMVPshop@users.noreply.github.com>
2021-07-29 17:58:56 -0500

    dfsfs

--- index.js
+++ index.js
@@ -10,13 +10,8 @@ const db = require('./sql/connect');
 // const clientsRouter = require("./Routers/clients");
 
 const app = express();
-<<<<<<< HEAD
 const port = process.env.PORT || 80;
 console.log("process.env.port ", port)
-=======
-const port = process.env.PORT || 8080;
-// const port = 3306;
->>>>>>> parent of 6cfad8a (updated port console logs)
 
 
 app.use(express.static('public'));
